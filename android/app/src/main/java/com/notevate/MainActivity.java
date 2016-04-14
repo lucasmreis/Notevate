092d4fd80d5d11340ca523d53d62295f69170a79
Lucas Reis <lucasmreis@gmail.com>
2016-04-13 22:38:45 -0300

    adding fabric + device

--- android/app/src/main/java/com/notevate/MainActivity.java
+++ android/app/src/main/java/com/notevate/MainActivity.java
@@ -1,6 +1,8 @@
 package com.notevate;
 
 import com.facebook.react.ReactActivity;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.smixx.fabric.FabricPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new RNDeviceInfo(),
+            new FabricPackage(this)
         );
     }
 }
--- index.android.js
+++ index.android.js
@@ -1,7 +1,17 @@
 'use strict'
 
+// import { Crashlytics } from 'react-native-fabric'
+
+import DeviceInfo from 'react-native-device-info'
+
+// Crashlytics.setUserName(DeviceInfo.getDeviceName())
+// Crashlytics.setUserIdentifier(DeviceInfo.getUniqueID())
+// Crashlytics.setBool('development', global.__DEV__)
+// Crashlytics.setString('device_manufacturer', DeviceInfo.getManufacturer())
+// Crashlytics.setString('device_model', DeviceInfo.getModel())
+
 import React, { AppRegistry } from 'react-native'
 
 import ViewFunction from './js/view'
 
-AppRegistry.registerComponent('Notevate', () => () => <ViewFunction />);
+AppRegistry.registerComponent('Notevate', () => () => <ViewFunction />)
